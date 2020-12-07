2e6eac3aabeb6f8c5a9e5d1359b60c45b9d92492
vickyywliu <vickyywliu@tencent.com>
2020-12-07 12:58:33 +0800

    version 1.0.0

---
+++ examples/asr/asrexample.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# 引用 SDK
+
+import time
+import sys
+import threading
+from datetime import datetime
+import json
+sys.path.append("../..")
+from common import credential
+from asr import speech_recognizer
+
+APPID = ""
+SECRET_ID = ""
+SECRET_KEY = ""
+ENGINE_MODEL_TYPE = "16k_zh"
+SLICE_SIZE = 640
+
+
+class MySpeechRecognitionListener(speech_recognizer.SpeechRecognitionListener):
+    def __init__(self, id):
+        self.id = id
+
+    def on_recognition_start(self, response):
+        print("%s|%s|OnRecognitionStart\n" % (
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S"), response['voice_id']))
+
+    def on_sentence_begin(self, response):
+        print("%s|%s|OnRecognitionSentenceBegin\n" % (
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S"), response['voice_id']))
+
+    def on_recognition_result_change(self, response):
+        rsp_str = json.dumps(response, ensure_ascii=False)
+        print("%s|%s|OnResultChange, rsp %s\n" % (datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S"), response['voice_id'], rsp_str))
+
+    def on_sentence_end(self, response):
+        rsp_str = json.dumps(response, ensure_ascii=False)
+        print("%s|%s|OnSentenceEnd, rsp %s\n" % (datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S"), response['voice_id'], rsp_str))
+
+    def on_recognition_complete(self, response):
+        print("%s|%s|OnRecognitionComplete\n" % (
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S"), response['voice_id']))
+
+    def on_fail(self, response):
+        rsp_str = json.dumps(response, ensure_ascii=False)
+        print("%s|%s|OnFail,message %s\n" % (datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S"), response['voice_id'], rsp_str))
+
+
+def process(id):
+    audio = "test.wav"
+    listener = MySpeechRecognitionListener(id)
+    credential_var = credential.Credential(SECRET_ID, SECRET_KEY)
+    recognizer = speech_recognizer.SpeechRecognizer(
+        APPID, credential_var, ENGINE_MODEL_TYPE,  listener)
+    recognizer.set_filter_modal(1)
+    recognizer.set_filter_punc(1)
+    recognizer.set_filter_dirty(1)
+    recognizer.set_need_vad(1)
+    recognizer.set_vad_silence_time(600)
+    recognizer.set_voice_format(1)
+    recognizer.set_word_info(1)
+    # recognizer.set_nonce("12345678")
+    recognizer.set_convert_num_mode(1)
+    try:
+        recognizer.start()
+        with open(audio, 'rb') as f:
+            content = f.read(SLICE_SIZE)
+            while content:
+                ret = recognizer.write(content)
+                if ret != 0:
+                    # 发送时发现websocket被关闭会重启websocket，重启后立即被关闭，可能是参数问题，结束识别
+                    raise RuntimeError('Sever shutdown the connection')
+                content = f.read(SLICE_SIZE)
+                time.sleep(0.02)
+    except Exception as e:
+        print(e)
+    finally:
+        recognizer.stop()
+
+
+def process_multithread(number):
+    thread_list = []
+    for i in range(0, number):
+        thread = threading.Thread(target=process, args=(i,))
+        thread_list.append(thread)
+        thread.start()
+
+    for thread in thread_list:
+        thread.join()
+
+
+if __name__ == "__main__":
+    process(0)
+    # process_multithread(20)
