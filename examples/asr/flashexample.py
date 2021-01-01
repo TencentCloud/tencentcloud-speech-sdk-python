b518622da3344ebb96111c4b274b23c3b4e574a2
yunjianfei <233602551@qq.com>
2021-01-01 12:21:41 +0800

    Update flashexample.py
    modify example

--- examples/asr/flashexample.py
+++ examples/asr/flashexample.py
@@ -9,27 +9,28 @@ sys.path.append("../..")
 from common import credential
 from asr import flash_recognizer
 
-APPID = ""
+APPID = "" 
 SECRET_ID = ""
 SECRET_KEY = ""
 ENGINE_TYPE = "16k_zh"
 
 if __name__=="__main__":
-    credential_var = credential.Credential(SECRET_ID, SECRET_KEY)
-    recognizer = flash_recognizer.FlashRecognizer(APPID, credential_var, ENGINE_TYPE)
-    recognizer.set_filter_modal(0)
-    recognizer.set_filter_punc(0)
-    recognizer.set_filter_dirty(0)
-    recognizer.set_voice_format("wav")
-    recognizer.set_word_info(0)
-    recognizer.set_convert_num_mode(1)
+    req = flash_recognizer.FlashRecognitionRequest(ENGINE_TYPE)
+    req.set_filter_modal(0)
+    req.set_filter_punc(0)
+    req.set_filter_dirty(0)
+    req.set_voice_format("wav")
+    req.set_word_info(0)
+    req.set_convert_num_mode(1)
 
+    credential_var = credential.Credential(SECRET_ID, SECRET_KEY)
+    recognizer = flash_recognizer.FlashRecognizer(APPID, credential_var)
     audio = "./test.wav"
     with open(audio, 'rb') as f:
         #读取音频数据
         data = f.read()
         #执行识别
-        resultData = recognizer.do_recognize(data)
+        resultData = recognizer.recognize(req, data)
         resp = json.loads(resultData)
         code = resp["code"]
         if code != 0:
