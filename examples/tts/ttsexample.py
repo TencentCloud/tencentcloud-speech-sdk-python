8893ae83a7d855a9503527c1c32d7949f36e5b11
Justin <justinli.ljt@gmail.com>
2023-06-25 16:40:10 +0800

    tts support websocket

--- examples/tts/ttsexample.py
+++ examples/tts/ttsexample.py
@@ -4,52 +4,161 @@
 import sys
 sys.path.append("../..")
 
-from datetime import datetime
 import wave
 import time
 import threading
-import json
 from common import credential
-from tts import speech_synthesizer
+from tts import speech_synthesizer_ws
+from common.log import logger
+from common.utils import is_python3
 
 
-APPID = ""
-SECRET_ID = ""
-SECRET_KEY = ""
+APPID = 0
+SECRET_ID = ''
+SECRET_KEY = ''
+
+TEXT = "欢迎使用腾讯云实时语音合成"
+VOICETYPE = 101001 # 音色类型
+CODEC = "pcm" # 音频格式：pcm/mp3
+SAMPLE_RATE = 16000 # 音频采样率：8000/16000
+ENABLE_SUBTITLE = True
 
 
-class MySpeechSynthesisListener(speech_synthesizer.SpeechSynthesisListener):
-    def __init__(self, id):
+class MySpeechSynthesisListener(speech_synthesizer_ws.SpeechSynthesisListener):
+    
+    def __init__(self, id, codec, sample_rate):
         self.start_time = time.time()
         self.id = id
+        self.codec = codec.lower()
+        self.sample_rate = sample_rate
 
-    def on_message(self, response):
-        cost = (time.time() - self.start_time) * 1000
-        print("%s|%s|on_message, size %d, cost %d\n" % (datetime.now().strftime(
-            "%Y-%m-%d %H:%M:%S"), response['session_id'], len(response['data']), cost))
+        self.audio_file = ""
+        self.audio_data = bytes()
+    
+    def set_audio_file(self, filename):
+        self.audio_file = filename
 
-    def on_complete(self, response):
-        cost = (time.time() - self.start_time) * 1000
-        print("%s|%s|on_complete, size %d, cost %d\n" % (datetime.now().strftime(
-            "%Y-%m-%d %H:%M:%S"), response['session_id'], len(response['data']), cost))
-        wavfile = wave.open('speech_synthesizer_output.wav', 'wb')
-        wavfile.setparams((1, 2, 16000, 0, 'NONE', 'NONE'))
-        wavfile.writeframes(response["data"])
-        wavfile.close()
+    def on_synthesis_start(self, session_id):
+        '''
+        session_id: 请求session id，类型字符串
+        '''
+        if is_python3():
+            super().on_synthesis_start(session_id)
+        else:
+            super(MySpeechSynthesisListener, self).on_synthesis_start(session_id)
+        
+        # TODO 合成开始，添加业务逻辑
+        if not self.audio_file:
+            self.audio_file = "speech_synthesis_output." + self.codec
+        self.audio_data = bytes()
 
-    def on_fail(self, response):
-        rsp_str = json.dumps(response, ensure_ascii=False)
-        print("%s|%s|on_fail, rsp %s\n" % (datetime.now().strftime(
-            "%Y-%m-%d %H:%M:%S"), response['session_id'], rsp_str))
+    def on_synthesis_end(self):
+        if is_python3():
+            super().on_synthesis_end()
+        else:
+            super(MySpeechSynthesisListener, self).on_synthesis_end()
 
+        # TODO 合成结束，添加业务逻辑
+        logger.info("write audio file, path={}, size={}".format(
+            self.audio_file, len(self.audio_data)
+        ))
+        if self.codec == "pcm":
+            wav_fp = wave.open(self.audio_file + ".wav", "wb")
+            wav_fp.setnchannels(1)
+            wav_fp.setsampwidth(2)
+            wav_fp.setframerate(self.sample_rate)
+            wav_fp.writeframes(self.audio_data)
+            wav_fp.close()
+        elif self.codec == "mp3":
+            fp = open(self.audio_file, "wb")
+            fp.write(self.audio_data)
+            fp.close()
+        else:
+            logger.info("codec {}: sdk NOT implemented, please save the file yourself".format(
+                self.codec
+            ))
+
+    def on_audio_result(self, audio_bytes):
+        '''
+        audio_bytes: 二进制音频，类型 bytes
+        '''
+        if is_python3():
+            super().on_audio_result(audio_bytes)
+        else:
+            super(MySpeechSynthesisListener, self).on_audio_result(audio_bytes)
+        
+        # TODO 接收到二进制音频数据，添加实时播放或保存逻辑
+        self.audio_data += audio_bytes
+
+    def on_text_result(self, response):
+        '''
+        response: 文本结果，类型 dict，如下
+        字段名       类型         说明
+        code        int         错误码（无需处理，SpeechSynthesizer中已解析，错误消息路由至 on_synthesis_fail）
+        message     string      错误信息
+        session_id  string      回显客户端传入的 session id
+        request_id  string      请求 id，区分不同合成请求，一次 websocket 通信中，该字段相同
+        message_id  string      消息 id，区分不同 websocket 消息
+        final       bool        合成是否完成（无需处理，SpeechSynthesizer中已解析）
+        result      Result      文本结果结构体
+
+        Result 结构体
+        字段名       类型                说明
+        subtitles   array of Subtitle  时间戳数组
+        
+        Subtitle 结构体
+        字段名       类型     说明
+        Text        string  合成文本
+        BeginTime   int     开始时间戳
+        EndTime     int     结束时间戳
+        BeginIndex  int     开始索引
+        EndIndex    int     结束索引
+        Phoneme     string  音素
+        '''
+        if is_python3():
+            super().on_text_result(response)
+        else:
+            super(MySpeechSynthesisListener, self).on_text_result(response)
+
+        # TODO 接收到文本数据，添加业务逻辑
+        result = response["result"]
+        subtitles = []
+        if "subtitles" in result and len(result["subtitles"]) > 0:
+            subtitles = result["subtitles"]
+
+    def on_synthesis_fail(self, response):
+        '''
+        response: 文本结果，类型 dict，如下
+        字段名 类型
+        code        int         错误码
+        message     string      错误信息
+        '''
+        if is_python3():
+            super().on_synthesis_fail(response)
+        else:
+            super(MySpeechSynthesisListener, self).on_synthesis_fail(response)
+
+        # TODO 合成失败，添加错误处理逻辑
+        err_code = response["code"]
+        err_msg = response["message"]
+        
 
 def process(id):
-    listener = MySpeechSynthesisListener(id)
+    listener = MySpeechSynthesisListener(id, CODEC, SAMPLE_RATE)
     credential_var = credential.Credential(SECRET_ID, SECRET_KEY)
-    text = "语音合成可自定义音量和语速，让发音更自然、更专业、更符合场景需求。满足将文本转化成拟人化语音的需求，打通人机交互闭环。支持多种音色选择，语音合成可广泛应用于语音导航、有声读物、机器人、语音助手、自动新闻播报等场景，提升人机交互体验，提高语音类应用构建效率。"
-    synthesizer = speech_synthesizer.SpeechSynthesizer(
-        APPID, credential_var, 1002, listener)
-    synthesizer.synthesis(text)
+    synthesizer = speech_synthesizer_ws.SpeechSynthesizer(
+        APPID, credential_var, listener)
+    synthesizer.set_text(TEXT)
+    synthesizer.set_voice_type(VOICETYPE)
+    synthesizer.set_codec(CODEC)
+    synthesizer.set_sample_rate(SAMPLE_RATE)
+    synthesizer.set_enable_subtitle(ENABLE_SUBTITLE)
+    
+    synthesizer.start()
+    # wait for processing complete
+    synthesizer.wait()
+
+    logger.info("process done")
 
 
 def process_multithread(number):
@@ -58,11 +167,12 @@ def process_multithread(number):
         thread = threading.Thread(target=process, args=(i,))
         thread_list.append(thread)
         thread.start()
+        print(i)
 
     for thread in thread_list:
         thread.join()
 
 
 if __name__ == "__main__":
-    process(0)
-    # process_multithread(20)
+    #process(0)
+    process_multithread(1)
---
+++ tts/speech_synthesizer_ws.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*-
+import sys
+import hmac
+import hashlib
+import base64
+import time
+import json
+import threading
+import websocket
+import uuid
+import urllib
+from common.log import logger
+from common.utils import is_python3
+
+
+_PROTOCOL = "wss://"
+_HOST = "tts.cloud.tencent.com"
+_PATH = "/stream_ws"
+_ACTION = "TextToStreamAudioWS"
+
+
+class SpeechSynthesisListener(object):
+    '''
+    '''
+    def on_synthesis_start(self, session_id):
+        logger.info("on_synthesis_start: session_id={}".format(session_id))
+
+    def on_synthesis_end(self):
+        logger.info("on_synthesis_end: -")
+
+    def on_audio_result(self, audio_bytes):
+        logger.info("on_audio_result: recv audio bytes, len={}".format(len(audio_bytes)))
+
+    def on_text_result(self, response):
+        session_id = response["session_id"]
+        request_id = response["request_id"]
+        message_id = response["message_id"]
+        result = response['result']
+        subtitles = []
+        if "subtitles" in result and len(result["subtitles"]) > 0:
+            subtitles = result["subtitles"]
+        logger.info("on_text_result: session_id={} request_id={} message_id={}\nsubtitles={}".format(
+            session_id, request_id, message_id, subtitles))
+
+    def on_synthesis_fail(self, response):
+        logger.error("on_synthesis_fail: code={} msg={}".format(
+            response['code'], response['message']
+        ))
+
+
+NOTOPEN = 0
+STARTED = 1
+OPENED = 2
+FINAL = 3
+ERROR = 4
+CLOSED = 5
+
+
+class SpeechSynthesizer:
+
+    def __init__(self, appid, credential, listener):
+        self.appid = appid
+        self.credential = credential
+        self.status = NOTOPEN
+        self.ws = None
+        self.wst = None
+        self.listener = listener
+
+        self.text = "欢迎使用腾讯云实时语音合成"
+        self.voice_type = 0
+        self.codec = "pcm"
+        self.sample_rate = 16000
+        self.volume = 0
+        self.speed = 0
+        self.session_id = ""
+        self.enable_subtitle = True
+
+    def set_voice_type(self, voice_type):
+        self.voice_type = voice_type
+
+    def set_codec(self, codec):
+        self.codec = codec
+
+    def set_sample_rate(self, sample_rate):
+        self.sample_rate = sample_rate
+
+    def set_speed(self, speed):
+        self.speed = speed
+
+    def set_volume(self, volume):
+        self.volume = volume
+    
+    def set_text(self, text):
+        self.text = text
+
+    def set_enable_subtitle(self, enable_subtitle):
+        self.enable_subtitle = enable_subtitle
+
+    def __gen_signature(self, params):
+        sort_dict = sorted(params.keys())
+        sign_str = "GET" + _HOST + _PATH + "?"
+        for key in sort_dict:
+            sign_str = sign_str + key + "=" + str(params[key]) + '&'
+        sign_str = sign_str[:-1]
+        if is_python3():
+            secret_key = self.credential.secret_key.encode('utf-8')
+            sign_str = sign_str.encode('utf-8')
+        else:
+            secret_key = self.credential.secret_key
+        hmacstr = hmac.new(secret_key, sign_str, hashlib.sha1).digest()
+        s = base64.b64encode(hmacstr)
+        s = s.decode('utf-8')
+        return s
+
+    def __gen_params(self, session_id):
+        self.session_id = session_id
+
+        params = dict()
+        params['Action'] = _ACTION
+        params['AppId'] = int(self.appid)
+        params['SecretId'] = self.credential.secret_id
+        params['ModelType'] = 1
+        params['VoiceType'] = self.voice_type
+        params['Codec'] = self.codec
+        params['SampleRate'] = self.sample_rate
+        params['Speed'] = self.speed
+        params['Volume'] = self.volume
+        params['SessionId'] = self.session_id
+        params['Text'] = self.text
+        params['EnableSubtitle'] = self.enable_subtitle
+
+        timestamp = int(time.time())
+        params['Timestamp'] = timestamp
+        params['Expired'] = timestamp + 24 * 60 * 60
+        return params
+
+    def __create_query_string(self, param):
+        if is_python3():
+            param['Text'] = urllib.parse.quote(param['Text'])
+        else:
+            param['Text'] = urllib.quote(param['Text'])
+        
+        param = sorted(param.items(), key=lambda d: d[0])
+
+        url = _PROTOCOL + _HOST + _PATH
+
+        signstr = url + "?"
+        for x in param:
+            tmp = x
+            for t in tmp:
+                signstr += str(t)
+                signstr += "="
+            signstr = signstr[:-1]
+            signstr += "&"
+        signstr = signstr[:-1]
+        return signstr
+
+    def start(self):
+        logger.info("synthesizer start: begin")
+
+        def _close_conn(reason):
+            ta = time.time()
+            self.ws.close()
+            tb = time.time()
+            logger.info("client has closed connection ({}), cost {} ms".format(reason, int((tb-ta)*1000)))
+
+        def _on_data(ws, data, opcode, flag):
+            # NOTE print all message that client received
+            # logger.info("data={} opcode={} flag={}".format(data, opcode, flag))
+            if opcode == websocket.ABNF.OPCODE_BINARY:
+                self.listener.on_audio_result(data) # <class 'bytes'>
+                pass
+            elif opcode == websocket.ABNF.OPCODE_TEXT:
+                resp = json.loads(data) # WSResponseMessage
+                if resp['code'] != 0:
+                    logger.error("server synthesis fail request_id={} code={} msg={}".format(
+                        resp['request_id'], resp['code'], resp['message']
+                    ))
+                    self.listener.on_synthesis_fail(resp)
+                    return
+                if "final" in resp and resp['final'] == 1:
+                    logger.info("recv FINAL frame")
+                    self.status = FINAL
+                    _close_conn("after recv final")
+                    self.listener.on_synthesis_end()
+                    return
+                if "result" in resp:
+                    if "subtitles" in resp["result"] and resp["result"]["subtitles"] is not None:
+                        self.listener.on_text_result(resp)
+                    return
+            else:
+                logger.error("invalid on_data code, opcode=".format(opcode))
+
+        def _on_error(ws, error):
+            if self.status == FINAL or self.status == CLOSED:
+                return
+            self.status = ERROR
+            logger.error("error={}, session_id={}".format(error, self.session_id))
+            _close_conn("after recv error")
+
+        def _on_close(ws, close_status_code, close_msg):
+            logger.info("conn closed, close_status_code={} close_msg={}".format(close_status_code, close_msg))
+            self.status = CLOSED
+
+        def _on_open(ws):
+            logger.info("conn opened")
+            self.status = OPENED
+            
+        session_id = str(uuid.uuid1())
+        params = self.__gen_params(session_id)
+        signature = self.__gen_signature(params)
+        requrl = self.__create_query_string(params)
+
+        if is_python3():
+            autho = urllib.parse.quote(signature)
+        else:
+            autho = urllib.quote(signature)
+        requrl += "&Signature=%s" % autho
+
+        self.ws = websocket.WebSocketApp(requrl, None,
+            on_error=_on_error, on_close=_on_close,
+            on_data=_on_data)
+        self.ws.on_open = _on_open
+        
+        self.wst = threading.Thread(target=self.ws.run_forever)
+        self.wst.daemon = True
+        self.wst.start()
+        self.status = STARTED
+        self.listener.on_synthesis_start(session_id)
+        
+        logger.info("synthesizer start: end")
+
+    def wait(self):
+        logger.info("synthesizer wait: begin")
+        if self.ws:
+            if self.wst and self.wst.is_alive():
+                self.wst.join()
+        logger.info("synthesizer wait: end")
