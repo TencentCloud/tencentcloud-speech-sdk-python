c6c317fe8654278040101f155ed0c5e515240fb8
Justin <justinli.ljt@gmail.com>
2023-07-07 11:36:59 +0800

    tts support emotion category

--- examples/tts/ttsexample.py
+++ examples/tts/ttsexample.py
@@ -22,6 +22,8 @@ VOICETYPE = 101001 # 音色类型
 CODEC = "pcm" # 音频格式：pcm/mp3
 SAMPLE_RATE = 16000 # 音频采样率：8000/16000
 ENABLE_SUBTITLE = True
+EMOTION_CATEGORY = "" # 仅支持多情感音色
+EMOTION_INTENSITY = 100
 
 
 class MySpeechSynthesisListener(speech_synthesizer_ws.SpeechSynthesisListener):
@@ -138,6 +140,10 @@ def process(id):
     synthesizer.set_codec(CODEC)
     synthesizer.set_sample_rate(SAMPLE_RATE)
     synthesizer.set_enable_subtitle(ENABLE_SUBTITLE)
+    if EMOTION_CATEGORY != "":
+        synthesizer.set_emotion_category(EMOTION_CATEGORY)
+        if EMOTION_INTENSITY != 0:
+            synthesizer.set_emotion_intensity(EMOTION_INTENSITY)
     
     synthesizer.start()
     # wait for processing complete
